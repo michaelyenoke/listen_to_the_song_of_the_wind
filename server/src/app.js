1e2ab463bc641eccd586f7bd16ce5b5e6ac3e86b
michaelyenoke <michaelyeone@hotmail.com>
2022-05-15 21:27:08 +0800

    update 20220515

--- server/src/app.js
+++ server/src/app.js
@@ -15,12 +15,12 @@ require('./routes')(app)
 
 
 // 同步資料庫
-//const User = require('./models/User') 
-//User.sync({}) // 資料庫會加上s
-// User.sync({force:true})
+const User = require('./models/User') 
+User.sync({}) // 資料庫會加上s
+//User.sync({force:true}) //會清空資料庫
 
 
 
 // 把 node 的 port 加回來; 和 rds 的 port 分開
-app.listen(8081);
+app.listen(8088);
 console.log('express is ok @@##!')
--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,7 +1,22 @@
 const User = require('../models/User') 
+const jwt = require('jsonwebtoken')
+const database_config = require('../config/database_config')
+
+
+
+// JWT token
+function jwtSignUser (user) {
+    const ONE_WEEK = 60*60*24*7
+    return jwt.sign(user, database_config.authentication.jwtSecret,{
+        expiresIn: ONE_WEEK
+    })
+}
+
+
 
 
 module.exports ={
+
     async register (req, res) {
         try {
             const user = await User.create(req.body)
@@ -11,5 +26,69 @@ module.exports ={
                 error:'This emal account is already in use. MOTHERFUCKER!'
             })
         }
+    },
+
+    async login(req,res){
+        try{
+            const {email, password} = req.body
+            const user = await User.findOne({
+                where:{
+                    email:email
+                }
+            })
+                        
+            //查看問題 ：
+            console.log('Console Log Here:')   
+            console.log('user',user.toJSON())   
+
+            if(!user){
+                return res.status(403).send({
+                    error:'The login information was incorrect.'
+                })
+            }
+
+            //const isPasswordValid = password === user.password
+            const isPasswordValid = await user.comparePassword(password)
+
+            //查看問題：
+            console.log(password, user.password)
+            console.log(isPasswordValid)
+            console.log('checked')
+
+
+
+            if(!isPasswordValid) {
+                return res.status(403).send({
+                    error:'The login information was incorrect'
+                })
+            }
+
+
+            const userJson = user.toJSON()
+            res.send({
+                user: userJson,
+                token: jwtSignUser(userJson) //加入這行
+            })
+
+
+            console.log('#test@@@@')
+            console.log(userJson)
+            console.log(jwtSignUser(userJson))
+
+
+
+        }catch(err){
+            res.status(500).send({
+                error:'An error has occured trying to log in.'
+            })
+
+        }
+
+
     }
+
+
+
+
+
 }
