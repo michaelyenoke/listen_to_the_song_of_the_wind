07073cad010092bcf5977dd0084a1c68f719e27e
michaelyenoke <michaelyeone@hotmail.com>
2022-08-30 17:12:39 +0800

    updtade

--- client/src/components/Globals/HeaderPage.vue
+++ client/src/components/Globals/HeaderPage.vue
@@ -63,7 +63,7 @@
       app
       color="white"
       dark
-      src="https://images.unsplash.com/photo-1523115191856-c203e76215a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80"
+      src="https://images.unsplash.com/photo-1475359524104-d101d02a042b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1294&q=80"
       prominent
     >
 
