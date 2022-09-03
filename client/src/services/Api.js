4ebfc979bf07b444443c220c0ac60a3dab267da0
michaelyenoke <michaelyeone@hotmail.com>
2022-09-04 00:29:35 +0800

    update api

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export default() => {
   return axios.create({
-	  //baseURL:"http://13.214.156.49:8088/"
-    baseURL:"http://127.0.0.1:8088/"
+    baseURL:"http://127.0.0.1:8088/" //on backend server
   });
 };
